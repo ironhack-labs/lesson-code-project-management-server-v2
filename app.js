faddd7b9ea94b8abda0ff57c32ec2a0b47ebfa81
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-29 19:36:35 +0200

    Starter code

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv/config");
+require("./db");
+const express = require("express");
+
+const app = express();
+
+require("./config")(app);
+
+// 👇 Start handling routes here
+const allRoutes = require("./routes");
+app.use("/api", allRoutes);
+
+require("./error-handling")(app);
+
+module.exports = app;
